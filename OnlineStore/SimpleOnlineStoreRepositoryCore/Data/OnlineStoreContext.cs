dd574e6f35e11a355ad6103b7722899e9c9173a2
Mateusz <mateusz@o2.pl>
2020-02-12 14:30:15 +0100

    SqlServer dependecy added

--- OnlineStore/SimpleOnlineStoreRepositoryCore/Data/OnlineStoreContext.cs
+++ OnlineStore/SimpleOnlineStoreRepositoryCore/Data/OnlineStoreContext.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -8,13 +9,20 @@ namespace SimpleOnlineStoreRepositoryCore.Data.Entities
 {
     class OnlineStoreContext : IdentityDbContext<AppUser, AppRole, int>
     {
-        public OnlineStoreContext(DbContextOptions<OnlineStoreContext> options)
+        private readonly IConfiguration _configuration;
+
+        public OnlineStoreContext(DbContextOptions<OnlineStoreContext> options, IConfiguration configuration)
             : base(options)
         {
-
+            _configuration = configuration;
         }
         DbSet<Store> Stores { get; set; }
         DbSet<Category> Categories { get; set; }
         DbSet<Product> Products { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(_configuration.GetConnectionString("OnlineStoreContext"));
+        }
     }
 }
