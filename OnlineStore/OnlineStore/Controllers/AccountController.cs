e371427bcc153aafbb483d9681241043bb946f3a
Mateusz <mateusz@o2.pl>
2020-02-13 09:23:21 +0100

    Added Home and Account controllers

---
+++ OnlineStore/OnlineStore/Controllers/AccountController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using SimpleOnlineStoreRepositoryCore.Data.Entities;
+
+namespace OnlineStore.Controllers
+{
+    public class AccountController : Controller
+    {
+        private UserManager<AppUser> UserManager { get; }
+        private SignInManager<AppUser> SignInManager { get; }
+        public AccountController(UserManager<AppUser> userManager, SignInManager<AppUser> signInManager)
+        {
+            UserManager = userManager;
+            SignInManager = signInManager;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
